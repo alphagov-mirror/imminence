36fff3301975371821d230ad796ec0dc66f69593
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-10-02 12:26:45 +0100

    Fix Style/HashSyntax offenses

--- db/migrate/20131115164931_delete_latest_data_sets_for_archived_services.rb
+++ db/migrate/20131115164931_delete_latest_data_sets_for_archived_services.rb
@@ -2,14 +2,14 @@ class DeleteLatestDataSetsForArchivedServices < Mongoid::Migration
   def self.up
     PlaceArchive.delete_all(
       conditions: {
-        :service_slug => "motorcycle-approved-training-bodies",
-        :data_set_version => {
+        service_slug: "motorcycle-approved-training-bodies",
+        data_set_version: {
           "$gte" => 114  } })
 
     PlaceArchive.delete_all(
       conditions: {
-        :service_slug => "number-plate-supplier",
-        :data_set_version => {
+        service_slug: "number-plate-supplier",
+        data_set_version: {
           "$gte" => 18  } })
   end
 
